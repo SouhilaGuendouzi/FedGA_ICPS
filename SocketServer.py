adc1da2a90371f37d28faf3728b911ab8928ce5b
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2022-04-13 09:13:36 +0200

    Update SocketServer.py

--- SocketServer.py
+++ SocketServer.py
@@ -5,6 +5,53 @@
 import socket
 import pickle 
 from FedAVG import FedAvg
+import select
+from _thread import *
+
+list_of_clients = []
+def clientthread(conn, addr):
+ 
+    # sends a message to the client whose user object is conn
+    conn.send("Welcome to this chatroom!")
+ 
+    while True:
+            try:
+                message = conn.recv(2048)
+                if message:
+ 
+                    """prints the message and address of the
+                    user who just sent the message on the server
+                    terminal"""
+                    print ("<" + addr[0] + "> " + message)
+ 
+                    # Calls broadcast function to send message to all
+                    message_to_send = "<" + addr[0] + "> " + message
+                    broadcast(message_to_send, conn)
+ 
+                else:
+                    """message may have no content if the connection
+                    is broken, in this case we remove the connection"""
+                    remove(conn)
+ 
+            except:
+                continue
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+ 
+"""Using the below function, we broadcast the message to all
+clients who's object is not the same as the one sending
+the message """
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients!=connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+ 
+                # if the link is broken, we remove the client
+    remove(clients)
 
 
 
@@ -12,21 +59,39 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('192.168.69.163', 15555))
 weights=[]
 num_clients=0
-list_of_clients = []
+
+socket.listen(100)
 while True:
-        socket.listen(5)
-        client, address = socket.accept()
+    conn, addr = server.accept()
+ 
+    """Maintains a list of clients for ease of broadcasting
+    a message to all available people in the chatroom"""
+    list_of_clients.append(conn)
+ 
+    # prints the address of the user that just connected
+    print (addr[0] + " connected")
+ 
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(clientthread,(conn,addr))   
 
-        print ("{} connected".format( address ))
 
-        response = client.recv(4096)
-        print(response.decode())
+
+        #client, address = socket.accept()
+
+        #print ("{} connected".format( address ))
+
+        #response = client.recv(4096)
         
-        if response != "":
-                weights[num_clients]= pickle.loads( response.decode())
-                print (weights[num_clients])
+        #print(response.decode())
+       # print(response)
+        #if response != "":
+             #   weights[num_clients]= pickle.loads( response.decode())
+              #  print (weights[num_clients])
             
-                clientO, addressO=  client, address
+             #   clientO, addressO=  client, address
 
 print ("Close")
 client.close()
+
+
