0c3993a83ff6d1ac8e84c0e264e8c783b7580425
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2022-05-16 15:47:06 +0200

    structure

--- utils/Graph.py
+++ utils/Graph.py
@@ -1,8 +1,5 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
-import math 
-
-
 
 
 def Plot_Graphes (args, accTrain,accTest,lossTrain,lossTest):
@@ -24,21 +21,21 @@ def Plot_Graphes (args, accTrain,accTest,lossTrain,lossTest):
 
   
       axis[0, 0].plot( epochs,clients_accuracy_train[i] ) 
-      axis[0, 0].set_title("Train Accuracy") 
+      axis[0, 0].set_title("{} Train Accuracy".format(args.aggr)) 
       axis[0, 0].legend(['Client {}'.format(j) for j in range(args.num_users)])
   
       axis[0, 1].plot(epochs,  clients_loss_train[i]) 
-      axis[0, 1].set_title("Train Loss") 
+      axis[0, 1].set_title("{} Train Loss".format(args.aggr)) 
       axis[0, 1].legend(['Client {}'.format(j) for j in range(args.num_users)])
   
       axis[1, 0].plot(epochs, clients_accuracy_test[i]) 
-      axis[1, 0].set_title("Test Accuracy") 
+      axis[1, 0].set_title("{} Test Accuracy".format(args.aggr)) 
       axis[1, 0].legend(['Client {}'.format(j) for j in range(args.num_users)])
    
       axis[1, 1].plot(epochs,  clients_loss_test[i]) 
-      axis[1, 1].set_title("Test Loss") 
+      axis[1, 1].set_title("{} Test Loss".format(args.aggr)) 
       axis[1, 1].legend(['Client {}'.format(j) for j in range(args.num_users)])
 
 
-    plt.savefig('./save/FedGAPer_30.png')
+    plt.savefig('./save/{}.png'.format(args.aggr))
     plt.show() 
